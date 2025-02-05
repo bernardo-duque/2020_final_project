f735f3a6dc538df09299a3bf171eee904a1951eb
bernardo_duque <bernardo_renno_duque@brown.edu>
2025-02-04 21:14:59 -0500

    concat data - error in filter

---
+++ Code/0_main.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(stringr)
+
+#change your data wd here
+wd_data <- "/Users/bernardoduque/Documents/Brown/Aulas/2nd semester/Applied Economics/Final Project/Data/Input/"
+
+source(A_concat_data.R)
---
+++ Code/A_concat_data.R
@@ -0,0 +1,22 @@
+### Selecting relevant variables and 
+### Concatenating all CSVs into a .rda
+
+setwd(wd_data)
+
+years <- c("2006-2007","2007-2009","2009-2011","2011-2012",
+            "2012-2014", "2014-2015","2015-2017","2017-2018","2018-2020-junho") 
+
+df <- NA
+crimes <- c("Apreensão de drogas","Furto de veículos","Homicídio doloso","Latrocínio (Roubo seguido de morte)",
+            "Morte por intervenção de agente do Estado"," Tentativa de homicídio","Roubo")
+
+for(year %in% years){
+    temp <- read.csv2(paste0(year,".csv"),, fileEncoding = "latin1",header=T)
+    temp < - temp %>%
+         select(id = controle, year = ano, month = mes, crime=titulo_do,place = cisp,
+                             city = municipio_fato,neighborhood = bairro_fato) %>%
+         filter(city == "Rio de Janeiro (Capital)",
+                            str_detect(crime,paste0(crimes,collapse="|")))
+
+    df <- rbind(df,temp)
+}
