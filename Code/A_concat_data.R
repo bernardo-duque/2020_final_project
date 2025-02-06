6addbfc6576c6813b7d25685bad8a5c1c0b47671
bernardo_duque <bernardo_renno_duque@brown.edu>
2025-02-05 22:10:07 -0500

    added some comments to the code

--- Code/A_concat_data.R
+++ Code/A_concat_data.R
@@ -3,13 +3,17 @@
 
 setwd(wd_data)
 
+# name of the files to iterate
 years <- c("2006-2007","2007-2009","2009-2011","2011-2012",
             "2012-2014", "2014-2015","2015-2017","2017-2018","2018-2020-junho") 
 
 df <- tibble()
+
+# crimes we are interested in
 crimes <- c("Apreensão de drogas","Furto de veículos","Homicídio doloso","Latrocínio (Roubo seguido de morte)",
             "Morte por intervenção de agente do Estado"," Tentativa de homicídio","Roubo")
 
+# read and concatenate all csvs selecting the relevant variables
 for(year in years){
     temp <- read.csv2(paste0(year,".csv"),, fileEncoding = "latin1",header=T)
     temp <- temp %>%
@@ -19,9 +23,9 @@ for(year in years){
                             str_detect(crime,paste0(crimes,collapse="|")))
 
     df <- rbind(df,temp)
-    rm(temp)
 }
 
+# adjust some variables
 df <- df %>%
     mutate(place_id = substr(place,2,3)) %>% 
     select(id,year,month,crime,place_id, neighborhood, date)
@@ -29,4 +33,8 @@ df <- df %>%
 # note that there can be more than one neighborhood in the same precint and one neighborhood can be in more than one precinct
 label(df$place_id) <- "Police precinct"
 
+# save the dataframe
 save(df, file = paste0(wd,"Input/df.rda"))
+
+# remove everything that is not the wd paths
+rm(list=setdiff(ls(), c("wd","wd_data")))
