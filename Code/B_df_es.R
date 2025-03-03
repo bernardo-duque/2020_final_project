8f0092cdcef13e8f139c3800119aa6c608eaa0a8
bernardo_duque <bernardo_renno_duque@brown.edu>
2025-03-03 14:01:31 -0500

    switched quick analysis to analysis instead of df_es

--- Code/B_df_es.R
+++ Code/B_df_es.R
@@ -1,26 +1,7 @@
 
 load(paste0(wd,"Input/df_date.rda"))
 
-n_events <- sum(df_date$event)
-
-mean_events_preccint <- df_date %>%
-  group_by(year,place_id) %>%
-  summarise(n_events = sum(event)) %>%
-  group_by(year) %>%
-  summarise(mean_events = mean(n_events),
-            sd_events = sd(n_events)) %>%
-  round(2)
-
-mean_events_preccint_month <- df_date %>%
-  group_by(year,month,place_id) %>%
-  summarise(n_events = sum(event)) %>%
-  group_by(year,month) %>%
-  summarise(mean_events = mean(n_events),
-            sd_events = sd(n_events)) %>%
-  round(2)
-
-
-  ## prepare data for event study
+## prepare data for event study
 # extract event day
 
 df_event_dates <- df_date %>%
